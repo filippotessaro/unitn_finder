a2020e678e76add6232a7b1f23c9026d0a062882
stefanobaggetto <stefano.baggetto@studenti.unitn.it>
2017-11-07 09:37:13 +0100

    aggiunto script js

---
+++ public/js/script.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const socket = io();
+var textarea = $("#message"),
+  chatForm = $("#chatform"),
+  chats = $(".chats"),
+  messageTimeSent = $(".timesent");
+
+textarea.keypress(function(e){
+
+  // Submit the form on enter
+  if(e.which == 13) {
+    e.preventDefault();
+    chatForm.trigger('submit');
+  }
+
+});
+
+chatForm.on('submit', function(e){
+
+  e.preventDefault();
+  var text = document.getElementById('message').value;
+
+  //outputYou.textContent = text;
+  var name='me';
+  createChatMessage(text, name, moment());
+  scrollToBottom();
+
+  socket.emit('chat message', text);
+  textarea.val("");
+});
+
+function setResponse(val) {
+
+    //$("#response").text(val);
+    outputBot.textContent = text(val);
+
+}
+
+socket.on('bot reply', function(replyText) {
+  //synthVoice(replyText);
+    console.log(replyText);
+    var name='';
+    if(replyText == '') replyText = '(No answer...)';
+    //outputBot.textContent = replyText;
+    createChatMessage(replyText, name, moment());
+		scrollToBottom();
+});
+
+
+setInterval(function(){
+
+		messageTimeSent.each(function(){
+			var each = moment($(this).data('time'));
+			$(this).text(each.fromNow());
+		});
+
+	},60000);
+
+	// Function that creates a new chat message
+	function createChatMessage(msg,user,now){
+
+		var who = '';
+
+		if(user==='me') {
+			who = 'me';
+		}
+		else {
+			who = 'you';
+		}
+
+		var li = $(
+			'<li class=' + who + '>'+
+				'<div class="image">' +
+					'<img src="../images/logo.png" />' +
+					'<b></b>' +
+					'<i class="timesent" data-time=' + now + '></i> ' +
+				'</div>' +
+				'<p></p>' +
+			'</li>');
+
+		// use the 'text' method to escape malicious user input
+		li.find('p').text(msg);
+		li.find('b').text(user);
+
+		chats.append(li);
+
+		messageTimeSent = $(".timesent");
+		messageTimeSent.last().text(now.fromNow());
+	}
+
+	function scrollToBottom(){
+		$("html, body").animate({ scrollTop: $(document).height()-$(window).height() },1000);
+	}
