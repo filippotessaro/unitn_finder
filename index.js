70d726ddce935834800f9f48f8d1fd6b10310424
enricobontorin <enricobontorin@gmail.com>
2017-11-07 09:54:11 +0100

    index.js: connessione al db e query di prova | package.json: moduli necessari

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const APIAI_TOKEN = process.env.APIAI_TOKEN;
 const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+const DB_TEST = process.env.DB_TEST;
 
 const express = require('express');
 const app = express();
@@ -23,6 +24,21 @@ io.on('connection', function(socket){
 
 const apiai = require('apiai')(APIAI_TOKEN);
 
+//richiedo modulo mongoose e schema persona
+var mongoose = require('mongoose');
+var Persona = require('./persona.js');
+
+//connessione al DB su mLab
+mongoose.connect(DB_TEST, {useMongoClient: true});//, {useMongoClient: true}
+const db = mongoose.connection;
+db.on('error', err => {
+  console.error(`Error while connecting to DB: ${err.message}`);
+});
+db.once('open', () => {
+  console.log('DB connected successfully!');
+});
+
+
 // Web UI
 app.get('/', (req, res) => {
   res.sendFile('index.html');
@@ -40,10 +56,30 @@ io.on('connection', function(socket) {
 
     apiaiReq.on('response', (response) => {
 
-        let aiText = response.result.fulfillment.speech;
-        
-        console.log('Bot reply: ' + aiText);
-        socket.emit('bot reply', aiText);
+      let aiText = response.result.fulfillment.speech;
+
+      if (response.result.parameters['unitn-prof-nomi']){
+
+          let prof_name = response.result.parameters['unitn-prof-nomi'];
+          let prof_surname = response.result.parameters['unitn-prof-cognomi'];
+          //let phone = '';
+          Persona.find({
+              nome: prof_name,
+              cognome: prof_surname
+          }).
+              exec(function(err, res){
+                  var phone = res[0].telefono;
+                  var email = res[0].mail;
+
+                  //console.log(res);
+              console.log(prof_name + ' ' + prof_surname + ' '+ email + ' ' + phone);
+              socket.emit('bot reply', prof_name + ' ' + prof_surname + ' ' + email + ' ' + phone);
+              });
+      }
+      else {
+          console.log('Bot reply: ' + aiText);
+          socket.emit('bot reply', aiText);
+      }
 
     });
 
